2536bf5ea27f032ad68f0e926a212e801a2db359
CreateSource <abnormalhare822@gmail.com>
2022-10-12 20:40:13 -0400

    FINALLY! GUI for elements, limited amount

--- client/main.py
+++ client/main.py
@@ -6,7 +6,7 @@ import numpy as np
 import random
 import copy
 
-import socket, pickle
+# import socket, pickle
 
 # INITIALIZE #
 pygame.init()
@@ -38,21 +38,25 @@ class WindowClass:
         self.center = (self.width/2, self.height/2)
         self.spriteList = []
         self.fontList = []
+        self.guiList = []
 
-Window = WindowClass((1920, 1080), "Alchemy Adventure Battle!")
+Window = WindowClass((1920 / 2, 1080 / 2), "Alchemy Adventure Battle!")
 
 screenX = 0
 screenY = 0
 
 class LoadSprite:
-    def __init__(self, image, size, x, y, type="sprite", name="test"):
-        self.image = pygame.image.load(image).convert_alpha()
+    def __init__(self, image, size, x, y, type="sprite", name="test", lID=0):
+        try:
+            self.image = pygame.image.load(image).convert_alpha()
+        except: self.image = image
         self.size = size
         self.width, self.height = size
         self.shape = pygame.transform.scale(self.image, self.size)
         self.name = name
         self.rect = self.shape.get_rect()
         self.type = type
+        self.render = True
         if type == "character":
             self.rect.center = (x, y)
             self.x, self.y = self.rect.center
@@ -60,9 +64,12 @@ class LoadSprite:
             self.movement = [-4, 4]
             self.movementFlags = [0, 0, 0, 0]
             self.items = []
-        elif type == "sprite" or type == "gui":
+            Window.spriteList.append(self)
+        elif type == "gui" or type == "topGui":
             self.rect.center = (x, y)
             self.x, self.y = self.rect.center
+            self.items = []
+            Window.spriteList.append(self)
         elif type == "textbox":
             self.x = Window.width - self.width
             self.y = Window.height - self.height
@@ -70,11 +77,17 @@ class LoadSprite:
             self.x, self.y = self.rect.center
             self.open = False
             self.eKey = False
+            Window.spriteList.append(self)
+        elif type == "topGuiElement":
+            self.render = False
+            self.id = lID
+            self.rect.center = (x, y)
+            self.x, self.y = self.rect.center
+            Window.guiList.append(self)
         else:
             self.rect.center = (x, y)
             self.x, self.y = self.rect.center
-        
-        Window.spriteList.append(self)
+            Window.spriteList.append(self)
 
 # try:
 #     # Create a TCP/IP socket
@@ -88,7 +101,8 @@ class LoadSprite:
 #     print('closing socket')
 #     sock.close()
 
-Main = LoadSprite("Main.png", (64, 64), 256, 256, "character", "Placeholder")
+Main = LoadSprite("Main.png", (64, 64), 256, 256, "character", "Player")
+Gui = LoadSprite("hotbar.png", (Window.width - 64, 64), Window.width / 2, Window.height - 32, "topGui", "hotbar")
 # textbox = LoadSprite("Textbox.png", (512, 320), "textbox", "textbox")
 
 resourceId = 0
@@ -100,6 +114,7 @@ class LoadResource:
         self.width, self.height = size
         self.name = name
         self.type = type
+        self.render = True
         if type == "spawner":
             self.image = pygame.image.load(image).convert_alpha()
             self.shape = pygame.transform.scale(self.image, self.size)
@@ -120,6 +135,7 @@ class LoadResource:
                 self.image = pygame.image.load(image).convert_alpha()
             self.shape = pygame.transform.scale(self.image, self.size)
             self.rect = self.shape.get_rect()
+            self.render = False
             
             self.rect.center = (x, y)
             self.x, self.y = self.rect.center
@@ -129,11 +145,11 @@ class LoadResource:
 
 airSpawner = LoadResource("spawner.png", (64, 64), 500, 300, "spawner", "Air Spawner")
 airSpawner.child = LoadResource("air.png", (32, 32), airSpawner.x + random.randint(-100, 100), airSpawner.y + random.randint(-100, 100), "element", "Air", airSpawner.id)
-earthSpawner = LoadResource("spawner.png", (64, 64), 500, 600, "spawner", "Air Spawner")
+earthSpawner = LoadResource("spawner.png", (64, 64), 500, 600, "spawner", "Earth Spawner")
 earthSpawner.child = LoadResource("earth.png", (32, 32), earthSpawner.x + random.randint(-100, 100), earthSpawner.y + random.randint(-100, 100), "element", "Earth", earthSpawner.id)
-fireSpawner = LoadResource("spawner.png", (64, 64), 800, 300, "spawner", "Air Spawner")
+fireSpawner = LoadResource("spawner.png", (64, 64), 800, 300, "spawner", "Fire Spawner")
 fireSpawner.child = LoadResource("fire.png", (32, 32), fireSpawner.x + random.randint(-100, 100), fireSpawner.y + random.randint(-100, 100), "element", "Fire", fireSpawner.id)
-waterSpawner = LoadResource("spawner.png", (64, 64), 800, 600, "spawner", "Air Spawner")
+waterSpawner = LoadResource("spawner.png", (64, 64), 800, 600, "spawner", "Water Spawner")
 waterSpawner.child = LoadResource("water.png", (32, 32), waterSpawner.x + random.randint(-100, 100), waterSpawner.y + random.randint(-100, 100), "element", "Water", waterSpawner.id)
 
 keydown = True
@@ -186,6 +202,9 @@ class GameClass:
                         Main.movementFlags[3] = 1
                     if event.key == pygame.K_e:
                         textbox.open = not textbox.open
+                    if event.key == pygame.K_q:
+                        Main.items = []
+                        self.redraw_elements()
 
             if Main.rect.x < 48:
                 screenX += 1 * (1 + (1 / Main.rect.x) * 120)
@@ -210,17 +229,17 @@ class GameClass:
             if Main.rect.x > Window.width - 48 - 64:
                 screenX -= 1 * (1 + (Window.width / Main.rect.x) * 5)
                 Main.speed[0] -= .3 * (1 + Window.width / Main.rect.x)
-                if Main.rect.x > Window.width - 30 and not Main.movementFlags[2] and Main.movementFlags[3]:
+                if Main.rect.x > Window.width - 40 - 64 and not Main.movementFlags[2] and Main.movementFlags[3]:
                     Main.speed[0] = 0
             elif Main.speed[0] != Main.movement[1] and Main.movementFlags[3]:
                 Main.speed[0] = Main.movement[1]
             elif not (Main.movementFlags[3] or Main.movementFlags[2]):
                 Main.speed[0] = 0
 
-            if Main.rect.y > Window.height - 48 - 64:
+            if Main.rect.y > Window.height - 48 - 64 - 32:
                 screenY -= 1 * (1 + (Window.height / Main.rect.y) * 5)
                 Main.speed[1] -= .3 * (1 + Window.height / Main.rect.y)
-                if Main.rect.y > Window.height - 30 - 64 and not Main.movementFlags[0] and Main.movementFlags[1]:
+                if Main.rect.y > Window.height - 40 - 64 - 32 and not Main.movementFlags[0] and Main.movementFlags[1]:
                     Main.speed[1] = 0
             elif Main.speed[1] != Main.movement[1] and Main.movementFlags[1]:
                 Main.speed[1] = Main.movement[1]
@@ -250,28 +269,42 @@ class GameClass:
                         # print(i.resProd)
                     elif i.type == "element":
                         self.elem_interact(i)
-                    
-                    if i.type != "character":
+                    if i.type != "character" and i.type != "topGui" and i.type != "gui" and i.type != "topGuiElement":
                         i.rect.center = (i.x + screenX, i.y + screenY)
 
-                    pygame.draw.rect(Window.screen, color["black"], i.rect, -1)
-                    Window.screen.blit(i.shape, i.rect)
+                    if i.render == True:
+                        pygame.draw.rect(Window.screen, color["black"], i.rect, -1)
+                        Window.screen.blit(i.shape, i.rect)
+                    else:
+                        i.render = True
             
+            for i in Window.guiList:
+                self.elem_interact(i)
+                pygame.draw.rect(Window.screen, color["black"], i.rect, -1)
+                Window.screen.blit(i.shape, i.rect)
+
             pygame.display.update()
     
     def elem_interact(self, element):
+        global Gui
         px, py = Main.rect.center
         ex, ey = element.rect.center
-        if abs(px - ex) < 48 and abs(py - ey) < 48:
+        if abs(px - ex) < 48 and abs(py - ey) < 48 and len(Main.items) < 40:
             Main.items.append(element)
             Window.spriteList.remove(element)
             for j in Window.spriteList:
                 try:
                     if j.id == element.parentId:
                         j.resProd -= 1
-                        print([i.name for i in Main.items])
                 except: continue
+            
+            self.elemsort(Main)
+            self.redraw_elements()
 
+            pop = pygame.mixer.Sound('pop.mp3')
+            pygame.mixer.Sound.set_volume(pop, 1.0)
+            pygame.mixer.Sound.play(pop)
+            pygame.mixer.Sound.fadeout(pop, 75)
 
     def text_interact(self, text):
         pygame.draw.rect(Window.screen, color["red"], textbox.rect, -1)
@@ -281,9 +314,34 @@ class GameClass:
         # print(textbox.rect)
         Font.render_to(Window.screen, (textbox.x - 200, textbox.y - 100), text, color["black"])
 
+    def elemsort(self, inventory):
+        order = ["Air", "Earth", "Fire", "Water"]
+        dummy = []
+        for i in range(len(order)):
+            for j in inventory.items:
+                if j.name == order[i]:
+                    dummy.append(j)
+        inventory.items = dummy
+
+    def redraw_elements(self):
+        incX = 0
+        incY = 0
+        lID = 0
+        Window.guiList = []
+        
+        for j in Main.items:
+            x = LoadSprite(j.image, (20, 20), Gui.x + incX - (Gui.size[0] / 2 - 35), Gui.y + incY, "topGuiElement", j.name + ".gui", lID)
+
+            incX += 16
+            if incX > 50 * 16:
+                incX = 0
+                incY += 16
+        
+        print([i.name for i in Window.guiList], [i.name for i in Main.items])
+
     def reorder_rendering(self):
         dummy = []
-        order = ["gui", "spawner", "element", "sprite", "character", "textbox"]
+        order = ["gui", "spawner", "element", "sprite", "character", "textbox", "topGui"]
         for i in range(len(order)):
             for j in Window.spriteList:
                 if j.type == order[i]:
